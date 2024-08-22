8d61614c41e3d2a98c9a26b12ac22755f953b70a
cmigliaccio00 <44817903+cmigliaccio00@users.noreply.github.com>
2024-08-22 12:05:51 +0200

    hardened (test), modifiche README

--- prj_G19/src/fault_list_manager/file_fault_list/selection_sort.rs
+++ prj_G19/src/fault_list_manager/file_fault_list/selection_sort.rs
@@ -1,17 +1,21 @@
-fn selection_sort(vet: Vec<i32>, N: i32){
-    let mut i=0;
+fn selection_sort(vet: Vec<i32>){
+    let mut N = vet.len();
     let mut j=0;
-    let Pos=0;
+    let mut min=0;
 
+    //-----------------------SELECTION SORT-------------------------
+    let mut i=0;
     while i<N-1{
-        Pos=i;
+        min=i;
         j=i+1;
         //Ricerca del minimo
         while j<N{
-            if Vec[j] < Vec[Pos]{   Pos=j;  }
+            if Vec[j] < Vec[min]{   Pos=j;  }
             j = j+1;
         }
-        if Pos!=i{      vet.swap(Pos,i);    }
+        //Scambio il minimo
+        vet.swap(min,i);
+        //Vado avanti
         i=i+1;
     }
 }
