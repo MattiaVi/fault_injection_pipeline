eccfb67d1f578eed7691f877e259a3084f9d048c
Alessandro Scavone <58234359+alexscavo@users.noreply.github.com>
2024-10-06 10:21:15 +0200

    Rimozione istruzioni commentate

--- prj_G19/src/injector/mod.rs
+++ prj_G19/src/injector/mod.rs
@@ -5,7 +5,7 @@ use crate::fault_list_manager::FaultListEntry;
 use crate::hardened::{Hardened, IncoherenceError};
 
 //TODO
-pub struct TestResult{
+pub struct TestResult {
     field: i32
 }
 
@@ -35,74 +35,60 @@ fn runner_selection_sort(variables: Arc<Variables>, tx_runner: Sender<&str>, rx_
     *variables.N.write().unwrap() = variables.vec.read().unwrap().len().into();
     tx_runner.send("i1");
     rx_runner.recv();
-    //println!("i1: {:?}", variables.N.read().unwrap());
 
     *variables.j.write().unwrap() = Hardened::from(0);
     tx_runner.send("i2");
     rx_runner.recv();
-        //println!("i2: {:?}", variables.N.read().unwrap());
 
     *variables.min.write().unwrap() = Hardened::from(10);
     tx_runner.send("i3");
     rx_runner.recv();
-        //println!("i3: {:?}", variables.N.read().unwrap());
 
     *variables.i.write().unwrap() = Hardened::from(0);
     tx_runner.send("i4");
     rx_runner.recv();
-        //println!("i4: {:?}", variables.N.read().unwrap());
+
 
     while *variables.i.read().unwrap() < (*variables.N.read().unwrap() - 1)? {
-       //println!("{:?}", variables.vec.read().unwrap());
         tx_runner.send("i5");
         rx_runner.recv();
-            //println!("{:?}", variables.N.read().unwrap());
 
         variables.min.write().unwrap().assign(*variables.i.read().unwrap())?;
         tx_runner.send("i6");
         rx_runner.recv();
-            //println!("{:?}", variables.N.read().unwrap());
 
         variables.j.write().unwrap().assign((*variables.i.read().unwrap() + 1)?)?;
         tx_runner.send("i7");
         rx_runner.recv();
-            //println!("{:?}", variables.N.read().unwrap());
+
 
         while *variables.j.read().unwrap() < *variables.N.read().unwrap() {
             tx_runner.send("i8");
             rx_runner.recv();
-                //println!("{:?}", variables.N.read().unwrap());
-
 
             if variables.vec.read().unwrap()[*variables.j.read().unwrap()] < variables.vec.read().unwrap()[*variables.min.read().unwrap()] {
                 tx_runner.send("i9");
                 rx_runner.recv();
-                    //println!("{:?}", variables.N.read().unwrap());
 
                 variables.min.write().unwrap().assign(*variables.j.read().unwrap())?;
                 tx_runner.send("i10");
                 rx_runner.recv();
-                    //println!("{:?}", variables.N.read().unwrap());
             }
 
             let tmp = (*variables.j.read().unwrap() + 1)?;  // necessario dato che non potrei fare j = j + 1, dato che dovrei acquisire un lock in lettura dopo averlo gia' acquisito sulla stessa variabile in scrittura
             variables.j.write().unwrap().assign(tmp)?;
             tx_runner.send("i11");
             rx_runner.recv();
-                //println!("{:?}", variables.N.read().unwrap());
-
         }
 
         variables.vec.write().unwrap().swap(variables.i.read().unwrap().inner()?, variables.min.read().unwrap().inner()?);
         tx_runner.send("i12");
         rx_runner.recv();
-            //println!("{:?}", variables.N.read().unwrap());
 
         let tmp = (*variables.i.read().unwrap() + 1)?;
         variables.i.write().unwrap().assign(tmp)?;
         tx_runner.send("i13");
         rx_runner.recv();
-            //println!("{:?}", variables.N.read().unwrap());
     }
 
 
